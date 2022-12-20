1779eb984a362e4a4228b04df2c0e9233436b056
biluadev <biluadevcod@gmail.com>
2022-12-20 16:51:06 +0100

    first version without slide ate home

---
+++ main.js
@@ -0,0 +1,39 @@
+
+// Mudar o header enquanto scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= navHeight) {
+        header.classList.add('scroll')
+    } else {
+        header.classList.remove('scroll')
+    }
+})
+
+// Menu btn
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
+
+// Back to top button
+const toTop = document.querySelector(".arrow-top");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 300) {
+        toTop.classList.add("active");
+        toTop.classList.add("pisca");
+    } else {
+        toTop.classList.remove("active");
+        toTop.classList.remove("pisca");
+    }
+})
+
